d362c061c300e00871a4ffcee86b1600b95c1992
Alesiso <lai2-01@itismeucci.com>
2023-04-13 09:22:37 +0200

    inserito nome

--- stampa.cpp
+++ stampa.cpp
@@ -1,7 +1,7 @@
 /*
 Scrivere un programma che stampi "n" volte la stringa "Bada come la fuma"  con "n" dato in input dall'utente.
 
-NOME COGNOME DATA
+Alesio Pan 13/4/2023
 */
 
 # include <iostream>
